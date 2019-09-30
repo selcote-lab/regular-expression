6a16b9a85bb29350f9791eef401352432777548b
zouhaire <zouhaire@idside.de>
2019-09-30 10:55:04 +0100

    tested more caracters boundaries

--- src/com/tonasolution/Main.java
+++ src/com/tonasolution/Main.java
@@ -19,5 +19,25 @@ public class Main {
 
         System.out.println(alphanumeric.replaceAll("[aeb]", "X"));
         System.out.println(alphanumeric.replaceAll("[abcD][ec]", "X"));
+
+        System.out.println("Harry".replaceAll("[Hh]arry", "test"));
+
+        String newAlphanumeric = "abcDeeeF12Ghhiiiijkl99z";
+        System.out.println(newAlphanumeric.replaceAll("[^ej]", "X"));
+        System.out.println(newAlphanumeric.replaceAll("[abcdef345678]", "X"));
+        System.out.println(newAlphanumeric.replaceAll("[a-fA-F3-9]", "X"));
+        System.out.println(newAlphanumeric.replaceAll("(?i)[a-f3-8]", "X"));
+        System.out.println(newAlphanumeric.replaceAll("[0-9]", "X"));
+        System.out.println(newAlphanumeric.replaceAll("\\d", "X"));
+        System.out.println(newAlphanumeric.replaceAll("\\D", "X"));
+
+        String hasWiteSpace = "I have blanks and \t a tab, and also a new line";
+        System.out.println(hasWiteSpace);
+        System.out.println(hasWiteSpace.replaceAll("\\s", ""));
+        System.out.println(hasWiteSpace.replaceAll("\\t", "X"));
+        System.out.println(hasWiteSpace.replaceAll("\\S", "X"));
+        System.out.println(newAlphanumeric.replaceAll("\\w", "X"));
+        System.out.println(newAlphanumeric.replaceAll("\\W", "X"));
+        System.out.println(newAlphanumeric.replaceAll("\\b", "X"));
     }
 }
